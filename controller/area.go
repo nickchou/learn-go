bf5df7982daac3d8ab61ea0070bca3ac244b0b65
zhouli <zl6216@ly.com>
2018-07-20 18:27:30 +0800

    gorm完善，插入数据万里

--- controller/area.go
+++ controller/area.go
@@ -7,13 +7,38 @@ import (
 	"log"
 
 	"github.com/PuerkitoBio/goquery"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/nickchou/learn-go/app"
 	"github.com/nickchou/learn-go/comm"
+	"github.com/nickchou/learn-go/model"
 )
 
 //AreaController 国家统计局数据获取控制器
 type AreaController struct {
 	app.App
+}
+
+//Goo 数据库一些初始化操作
+func (con *AreaController) Goo() {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("数据库连接成功")
+		//检查表
+		if db.HasTable(&model.Area{}) {
+			fmt.Println("存在表Area")
+		} else {
+			fmt.Println("不存在表Area")
+			//AutoMigrate只会创建表、缺失的列和索引。不会改变现有列的数据类型或者删除数据
+			//http://doc.gorm.io/database.html#migration
+			db.AutoMigrate(&model.Area{})
+			fmt.Println("创建表Area")
+		}
+	}
+
 }
 
 //Prov 获取国家统计局的行政规划数据
@@ -25,10 +50,31 @@ func (con *AreaController) Prov() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//创建数据库连接
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//AutoMigrate只会创建表、缺失的列和索引。不会改变现有列的数据类型或者删除数据
+	//http://doc.gorm.io/database.html#migration
+	db.AutoMigrate(&model.Area{})
+	//找文档
 	doc.Find(".provincetr td a").Each(func(i int, se *goquery.Selection) {
 		url, _ := se.Attr("href")
 		url = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/" + url
 		city, _ := comm.ToGBK(se.Text())
+		//插入数据
+		area := model.Area{Aid: 0, Pid: 100000, Level: 1, Name: city, Lng: 2223.322321332, Lat: 32131.12345, URL: url}
+		//主键为空返回true
+		issucc := db.NewRecord(&area)
+		if issucc {
+			//插入数据
+			db.Create(&area)
+		}
+		//由于插入数据后有主键所以会返回false
+		issucc2 := db.NewRecord(&area)
+		fmt.Println(fmt.Sprintf("%v,%v,%v", issucc, issucc2, area.ID))
 		//造返回字符串
 		buffer.WriteString(fmt.Sprintf("%v,%v,%v<br/>", i+1, city, url))
 	})
--- model/Area.go
+++ model/Area.go
@@ -2,18 +2,25 @@ package model
 
 //Area 国家行政区划相关
 type Area struct {
-	ID          int     //国家地区ID
-	Pid         int     //父PID,确定归属
-	level       int     //层级 0:国家 1：省  2：市 3：区/县  4:镇  5：村委会
-	Name        string  //名字
-	SimName     string  //名字简写
-	NamePath    string  //层级全名
-	SimNamePath string  //缩写层级全名
-	TelCode     string  //电话区号
-	ZipCode     string  //邮编
-	Pinyin      string  //拼音
-	SimPinyin   string  //拼音缩写
-	FirstPinyin string  //拼音首字母
-	Lng         float32 //经度
-	Lat         float32 //维度
+	ID           int64   `gorm:"column:id;primary_key;AUTO_INCREMENT"` //自增ID
+	Aid          int64   `gorm:"column:aid;not null"`                  //国家地区ID
+	Pid          int64   `gorm:"column:pid;not null"`                  //父PID,确定归属
+	Level        int     `gorm:"column:level;not null"`                //层级 0:国家 1：省  2：市 3：区/县  4:镇  5：村委会
+	Name         string  `gorm:"column:name;not null"`                 //名字
+	SimName      string  `gorm:"column:sim_name;not null"`             //名字简写
+	NamesPath    string  `gorm:"column:names_path;not null"`           //层级全名
+	SimNamesPath string  `gorm:"column:sim_names_path;not null"`       //缩写层级全名
+	TelCode      string  `gorm:"column:tel_code;not null"`             //电话区号
+	ZipCode      string  `gorm:"column:zip_code;not null"`             //邮编
+	Pinyin       string  `gorm:"column:pinyin;not null"`               //拼音
+	SimPinyin    string  `gorm:"column:sim_pinyin;not null"`           //拼音缩写
+	FirstPinyin  string  `gorm:"column:first_pinyin;not null"`         //拼音首字母
+	Lng          float32 `gorm:"column:lng;not null"`                  //经度
+	Lat          float32 `gorm:"column:lat;not null"`                  //维度
+	URL          string  `gorm:"column:url;not null"`                  //官方url地址
+}
+
+//TableName 更改数据库表名
+func (Area) TableName() string {
+	return "area_asia"
 }
